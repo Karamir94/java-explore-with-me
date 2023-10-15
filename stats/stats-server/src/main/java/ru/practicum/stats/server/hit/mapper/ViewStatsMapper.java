9214a5f56a3f6764bf77d4b576e1ec425430abb3
Vladimir <legendking@yandex.ru>
2023-10-15 22:21:57 +0700

    Добавить сервис по сбору статистики

---
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/hit/mapper/ViewStatsMapper.java
@@ -0,0 +1,15 @@
+package ru.practicum.stats.server.hit.mapper;
+
+import ru.practicum.stats.dto.ViewStatsDto;
+import ru.practicum.stats.server.hit.model.ViewStats;
+
+public class ViewStatsMapper {
+
+    public static ViewStatsDto toViewStatsDto(ViewStats viewStats) {
+        return ViewStatsDto.builder()
+                .hits(viewStats.getHits())
+                .uri(viewStats.getUri())
+                .app(viewStats.getApp())
+                .build();
+    }
+}
---
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/hit/model/ViewStats.java
@@ -0,0 +1,13 @@
+package ru.practicum.stats.server.hit.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ViewStats {
+
+    private final String uri;
+    private final String app;
+    private final Long hits;
+}
